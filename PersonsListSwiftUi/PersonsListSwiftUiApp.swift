18b47d18c8e768cebdfb926083de2a3b02cff635
ILIA <dubenskiiilia@gmail.com>
2022-12-22 11:22:06 +0100

    Initial Commit

---
+++ PersonsListSwiftUi/PersonsListSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonsListSwiftUiApp.swift
+//  PersonsListSwiftUi
+//
+//  Created by Илья Дубенский on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonsListSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
